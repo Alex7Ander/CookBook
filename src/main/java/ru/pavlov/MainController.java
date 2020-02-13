e02c485e7198ec09c544de38576d8d0b6f20c76a
Alex7Ander <alex.ander.1991@mail.ru>
2020-02-13 06:43:21 +0300

    Start remaking templates of ui using thymeleaf

--- src/main/java/ru/pavlov/MainController.java
+++ src/main/java/ru/pavlov/MainController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,17 +38,17 @@ public class MainController {
 	
 // /user/**	
 	@GetMapping("user/cookbook")
-	public String cookbook(Map<String, Object> model) {
+	public String cookbook(Model model) {
 		Iterable<Recipe> recipes = recipeRepo.findAll();
-		model.put("recipes", recipes);
+		model.addAttribute("recipes", recipes);
 		return "cookbook";
 	}
 	
 	@GetMapping("user/recipe")
-	public String recipe(@RequestParam(name="name", required=true) String name, Map<String, Object> model) {
+	public String recipe(@RequestParam(name="name", required=true) String name, Model model) {
 		List<Recipe> recipes = recipeRepo.findByName(name);
 		Recipe recipe = recipes.get(0);
-		model.put("recipe", recipe);
+		model.addAttribute("recipe", recipe);
 		return "recipe";
 	}
 	
@@ -64,11 +65,11 @@ public class MainController {
 	//---------------------------------
 	
 	@PostMapping("user/saverecipe")
-	public String saverecipe(@RequestParam String name, @RequestParam String type, @RequestParam String tagline, @RequestParam String text, Map<String, Object> model) {
+	public String saverecipe(@RequestParam String name, @RequestParam String type, @RequestParam String tagline, @RequestParam String text, Model model) {
 		Recipe recipe = new Recipe(name, type, tagline, text);
 		recipeRepo.save(recipe);
 		Iterable<Recipe> recipes = recipeRepo.findAll();
-		model.put("recipes", recipes);
+		model.addAttribute("recipes", recipes);
 		return "cookbook";
 	}
 	
