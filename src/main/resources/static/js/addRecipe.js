2832d56ceab14c42584db7d66ffc490240b90cbc
Alex7Ander <alex.ander.1991@mail.ru>
2020-04-26 22:40:46 +0300

    Adding recipe to js Map. Should be done:deleting and sending to server

--- src/main/resources/static/js/addRecipe.js
+++ src/main/resources/static/js/addRecipe.js
@@ -1,3 +1,96 @@
+var ingredientsMap = new Map();
+ 
+let currentlyUploadedPhoto;
+var count = 0;
+
+//
+function getCurrentLyUploadedPhoto(event){
+	let photoFileLoader = document.getElementById('photoLoader');
+	currentlyUploadedPhoto = photoFileLoader.files[0];
+	let output  = document.getElementById('uploadedPhoto');
+	output.src = URL.createObjectURL(currentlyUploadedPhoto); 
+}
+
+function addPhotoToPhotoList(event){
+	event.stopPropagation(); // остановка всех текущих JS событий
+	event.preventDefault();  // остановка дефолтного события для текущего элемента
+	let photoData = new FormData();
+	photoData.append('photo', currentlyUploadedPhoto);
+	$.ajax({type: "POST", url: "/user/addRecipePhoto", cache: false, dataType: 'json', contentType: false, processData : false, data: photoData,
+		success: function(respond, status, jqXHR){
+			if( typeof respond.error === 'undefined' ){
+				showUploadedPhotoOnMainPage(respond);							
+			}
+			else{
+				alert('Error: ' + respond.data);
+			}
+		}, 
+		error: function(respond, status, jqXHR){
+			alert('Не удалось загрузить фото на сервер: ' + status);
+		},
+		complete: function(){
+			//showUploadedPhotoOnMainPage('respond');
+			PopUpHide('add_photo_popup');
+		}
+	});
+}
+
+function showUploadedPhotoOnMainPage(code){
+	var element = document.getElementById('photoList');
+	var newPhotoCard = document.createElement('div');
+	newPhotoCard.id = count;
+	newPhotoCard.className = "card";
+	element.append(newPhotoCard);	
+	// Image
+	var image = document.createElement('img');
+	var photoPath = document.getElementById('uploadedPhoto').src;
+	image.src = photoPath;
+	image.className = "imageContainer";
+	newPhotoCard.append(image);
+	// Hidden input with hashcode
+	var codeInput = document.createElement('input');
+	codeInput.type = "hidden";
+	codeInput.id="hidden_" + count;
+	codeInput.className = "hiddenInput";
+	codeInput.value = code;
+	newPhotoCard.append(codeInput);
+	// Delete link
+	var deleteBtn = document.createElement('input');
+	deleteBtn.type="button";
+	deleteBtn.value = "Удалить";
+	deleteBtn.className = "btn btn-primary";
+	deleteBtn.setAttribute('onclick', 'deletePhotoCard()');
+	deleteBtn.id = count;
+	newPhotoCard.append(deleteBtn);
+	count++;
+}
+
+function deletePhotoCard(){
+	let ch = event.target.parentNode;
+	let index = ch.id;
+	let currentHiddenInput = document.getElementById("hidden_" + index);
+	let code = currentHiddenInput.value;
+
+	let photoData = new FormData();
+	photoData.append('code', code);
+	
+	$.ajax({type: "POST", url: "/user/deleteRecipePhoto", cache: false, dataType: 'json', contentType: false, processData : false, data: photoData,
+		success: function(respond, status, jqXHR) {
+			if (typeof respond.error === 'undefined') {	
+				let parent = document.getElementById('photoList');
+				parent.removeChild(ch);	
+				alert('Фотография успешно удалена');						
+			}
+			else {
+				alert('Error: ' + respond.data);
+			}
+		}, 
+		error: function(respond, status, jqXHR) {
+			alert('Ошибка при удалении фотографии: ' + status);
+		}
+	});
+}
+
 function getIngrListFromServer(){
     req = new asyncRequest();
     element = document.getElementById('ingrList');
@@ -34,53 +127,63 @@ function saveNewIngredient(){
 
 function addExistingIngrToTable(){
     try{
-        var ingr = new ingredient();
-        var selectType = document.getElementById("ingrType");
-        var cType = selectType.value;
-        var selectName = document.getElementById("ingrName");
-        var cName = selectName.value;
-        ingr.getDataFromServer(cName, cType);
-    
-        //Adding line to table
-        var tbody = document.getElementById('ingrTable').getElementsByTagName("TBODY")[0];
-        var row = document.createElement("TR");	
-        var resultCalText = document.createElement('span');
-        var volumeInput = document.createElement('input');
-        volumeInput.oninput = function() {
-            var resultCalValue = ingr.getCalorificValue() * volumeInput.value / 100;
-            resultCalText.innerHTML = "<b>" + resultCalValue + "</b>";
-        }
-        var deleteBtn = document.createElement('input')
-        deleteBtn.type = 'button';
-        deleteBtn.value= 'Удалить';
-        deleteBtn.setAttribute('onclick', 'deleteIngrFromTable()');
-        var col1 = document.createElement("TD");
-        col1.appendChild(document.createTextNode(ingr.name));
-        var col2 = document.createElement("TD");
-        col2.appendChild(document.createTextNode(ingr.getCalorificValue()));
-        var col3 = document.createElement("TD");
-        col3.appendChild(volumeInput);
-        var col4 = document.createElement("TD");
-        col4.appendChild(resultCalText);
-        var col5 = document.createElement("TD");		
-        col5.appendChild(deleteBtn);	
-        row.appendChild(col1);
-        row.appendChild(col2);
-        row.appendChild(col3);
-        row.appendChild(col4);
-        row.appendChild(col5);
-        tbody.appendChild(row);
+		var ingr = new ingredient();
+		var selectType = document.getElementById("ingrType");
+		var cType = selectType.value;
+		var selectName = document.getElementById("ingrName");
+		var cName = selectName.value;
+		ingr.getDataFromServer(cName, cType);
+
+		//Adding line to table
+		var tbody = document.getElementById('ingrTable').getElementsByTagName("TBODY")[0];
+		var row = document.createElement("TR");	
+		var resultCalText = document.createElement('span');
+		var volumeInput = document.createElement('input');
+		var hiddenNameInput = document.createElement('input');
+		hiddenNameInput.setAttribute('type', 'hidden');
+		hiddenNameInput.setAttribute('value', ingr.name);
+		volumeInput.oninput = function() {
+			var resultCalValue = ingr.getCalorificValue() * volumeInput.value / 100;
+			resultCalText.innerHTML = "<b>" + resultCalValue + "</b>";
+		}
+		var deleteBtn = document.createElement('input')
+		deleteBtn.type = 'button';
+		deleteBtn.value= 'Удалить';
+		deleteBtn.setAttribute('onclick', 'deleteIngrFromTable()');
+		var col1 = document.createElement("TD");
+		col1.appendChild(document.createTextNode(ingr.name));
+		var col2 = document.createElement("TD");
+		col2.appendChild(document.createTextNode(ingr.getCalorificValue()));
+		var col3 = document.createElement("TD");
+		col3.appendChild(volumeInput);
+		var col4 = document.createElement("TD");
+		col4.appendChild(resultCalText);
+		var col5 = document.createElement("TD");		
+		col5.appendChild(deleteBtn);	
+		
+
+		row.appendChild(col1);
+		row.appendChild(col2);
+		row.appendChild(col3);
+		row.appendChild(col4);
+		row.appendChild(col5);
+		tbody.appendChild(row);
+
+		//Adding ingredient to map
+		ingredientsMap.set(ingr.name, 0);
     }
     catch(e){
-        alert("Ошибка при попытке добавить ингредиент");
+		alert("Ошибка при попытке добавить ингредиент");
     }
     //Closing pop-up window
     PopUpHide('add_ingr_popup');		
 }
 
 function deleteIngrFromTable(){
-    var ch = event.target.parentNode;
-    ch.parentNode.remove();
+	var ch = event.target.parentNode;	
+	var ingrName = ch.getElementByTagName('input').value;
+	ingredientsMap.delete(ingrName);
+	ch.parentNode.remove();
 }
 
 function sendPhoto(){
@@ -94,16 +197,8 @@ function sendPhoto(){
     }
     var inputFile = document.getElementById('photoLoader');
     var body = "photo=" + inputFile.value;	
-    //var boundary = String(Math.random()).slice(2);
-    //var boundaryMiddle = '--' + boundary + '\r\n';
-    //var boundaryLast = '--' + boundary + '--\r\n';
-
-    //var formData = new FormData();
-    //formData.append("photo", inputFile.value);
     req.open("Get", "/user/addRecipePhoto?"+body, false);
-    //req.setRequestHeader('Content-Type', 'multipart/form-data boundary=' + boundary); //  application/x-www-form-urlencoded
     req.send();
-    //element.innerHTML = "Список ингредиентов загружается...";
 }
 
 function uploadFileToPage(event) {
